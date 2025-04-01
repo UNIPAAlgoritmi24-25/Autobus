88ad995f7710739d5027d75ace489c9e968d4013
Andrea Liuzzo <andrealiuzzo95@gmail.com>
2025-04-01 15:46:22 +0100

    Implementato algoritmo di ordinamento bubble_sort

--- Ordinamento.py
+++ Ordinamento.py
@@ -26,3 +26,10 @@ def counting_sort(arr):
             arr.append(i)
             count[i] -= 1
     return arr
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
