ebce1bda302d11629434f76a31d5dc81dc4f03bd
D4d0-0 <davide.dovico@community.unipa.it>
2025-05-15 11:31:59 +0200

    Update Ordinamento.py

--- Ordinamento.py
+++ Ordinamento.py
@@ -36,8 +36,25 @@ def counting_sort(arr):
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
-        for j in range(n - i - 1):  # oppure range(0, n - i - 1), è equivalente
+        for j in range(n - i - 1):  
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
+def insertion_sort(arr):
+    n = len(arr)        
+     
+    if n <= 1:
+        return      
+
+    for i in range(1, n):      
+        key = arr[i]        
+        j = i-1
+        
+        while j >= 0 and key < arr[j]:      
+            arr[j+1] = arr[j]       
+            j -= 1
+            
+        arr[j+1] = key     
+
+    return arr
