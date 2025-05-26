51dd2e37ac37efa0e56a736c4636f2390775c963
D4d0-0 <davide.dovico@community.unipa.it>
2025-05-26 17:19:47 +0200

    Update Grafi.py
    aggiunto dijkstra

--- Grafi.py
+++ Grafi.py
@@ -151,6 +151,33 @@ class Grafo:
             u.colore = COLORE.NERO
 
         return output
+        
+    def dijkstra(self, sorgente):
+        distanze = {n: float('inf') for n in self.nodi}
+        precedente = {n: None for n in self.nodi}
+        distanze[sorgente] = 0
+        heap = [(0, sorgente)]
+
+        while heap:
+            distanza_u, u = heapq.heappop(heap)
+            for arco in self.archi:
+                if arco.partenza == u:
+                    v = arco.destinazione
+                    peso = arco.peso
+                    if distanze[u] + peso < distanze[v]:
+                        distanze[v] = distanze[u] + peso
+                        precedente[v] = u
+                        heapq.heappush(heap, (distanze[v], v))
+
+        return distanze, precedente
+
+    def ricostruisci_cammino(self, precedente, destinazione):
+        cammino = []
+        while destinazione:
+            cammino.insert(0, destinazione)
+            destinazione = precedente[destinazione]
+        return cammino
+
 
 
 n1 = Nodo("1")
